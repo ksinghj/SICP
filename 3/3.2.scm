85d62d1acfc16d959d244ea033aee3b64494fd62
ksinghj <kartarsjabanda@gmail.com>
2022-06-20 09:54:02 +0100

    mutable data

--- 3/3.2.scm
+++ 3/3.2.scm
@@ -2,7 +2,7 @@
     (let ((call-count 0))
         (cond ((eq? mf 'how-many-calls?) call-count))
               ((eq? mf 'reset-count) (set! call-count 0))
-              (else (set! call-count (+ 1 call-count)) (f))))
+              ))
 
 (define (double number)
     (* 2 number))
@@ -10,5 +10,3 @@
 (define d (make-monitored double))
 ; (d 2)
 ; (d 'how-many-calls?)
-
-; Didn't set the call-count++ !
