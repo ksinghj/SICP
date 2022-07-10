24ce397aa47318628680383c42382a55444fcf7c
ksinghj <kartarsjabanda@gmail.com>
2022-07-10 19:14:17 +0100

    3.15

--- 3/3.2.scm
+++ 3/3.2.scm
@@ -1,12 +1,15 @@
-(define (make-monitored f)
+(define (make-monitored f . args)
     (let ((call-count 0))
-        (cond ((eq? mf 'how-many-calls?) call-count))
-              ((eq? mf 'reset-count) (set! call-count 0))
-              ))
+        (lambda (mf)
+            (begin (
+                (f args)
+                (set! call-count (+ 1 call-count))
+                (cond ((eq? mf 'how-many-calls?) call-count))
+                      ((eq? mf 'reset-count) (set! call-count 0)))))))
 
 (define (double number)
     (* 2 number))
 
 (define d (make-monitored double))
-; (d 2)
-; (d 'how-many-calls?)
+(d 2)
+(d 'how-many-calls?)
